f1fd55603264897973392cf8d7562aec43ac1a24
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 14:37:00 -0500

    Setup lazy loading of sub-app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './apps/landing/landing.module#LandingModule'
+  },
+  {
+    path: 'micro-upload',
+    loadChildren: './apps/micro-upload/micro-upload.module#MicroUpload'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
