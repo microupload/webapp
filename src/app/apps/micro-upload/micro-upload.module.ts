5ade9ce354cbc7292da71903cff67ec4ec649648
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 14:39:25 -0500

    Added basic routing in MicroUploadModule

--- src/app/apps/micro-upload/micro-upload.module.ts
+++ src/app/apps/micro-upload/micro-upload.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { MicroUploadComponent } from './micro-upload.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: MicroUploadComponent
+  }
+];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [MicroUploadComponent]
 })
