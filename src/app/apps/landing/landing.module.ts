f99aebadee8c0f0e1820f9572de41b0a596e36f4
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 14:44:58 -0500

    Added a HomeComponent page to LandingModule

--- src/app/apps/landing/landing.module.ts
+++ src/app/apps/landing/landing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingComponent } from './landing.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,6 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [LandingComponent]
+  declarations: [LandingComponent, HomeComponent]
 })
 export class LandingModule { }
