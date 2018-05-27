f473fd29b81a73bcbd38e9a46053ab02d16d663a
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 14:37:13 -0500

    Setup routing in LandingModule

--- src/app/apps/landing/landing.module.ts
+++ src/app/apps/landing/landing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { LandingComponent } from './landing.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [LandingComponent]
 })
