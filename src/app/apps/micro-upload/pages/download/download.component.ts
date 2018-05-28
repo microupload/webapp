93c45f168fb4d5a211a1b5318b6bb490f0f85569
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-28 00:08:14 -0500

    Handle errors better in DownloadComponent

--- src/app/apps/micro-upload/pages/download/download.component.ts
+++ src/app/apps/micro-upload/pages/download/download.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FileService } from '../../services/file.service';
 import { CryptoService } from '../../../../modules/crypto/services/crypto.service';
+import { createChangeDetectorRef } from '@angular/core/src/view/refs';
 
 @Component({
   selector: 'app-download',
@@ -44,8 +45,8 @@ export class DownloadComponent implements OnInit {
 
   public async onKeyChange($event) {
     this.priv = $event.target.files[0];
+    this.message = 'Your file is being decrypted!';
     await this.crypto.setPriv(this.priv);
-
     this.createDownloadLink(this.data, this.metadata);
   }
 
@@ -56,12 +57,18 @@ export class DownloadComponent implements OnInit {
   private async createDownloadLink(data, metadata) {
     const filename = metadata.filename;
     const type = metadata.mimetype;
-    const blob = await this.crypto.decrypt(new File([data], metadata.filename, { type }));
+    let file: File;
+    try {
+      file = await this.crypto.decrypt(new File([data], metadata.filename, { type }));
+    } catch (e) {
+      this.message = e.message;
+      console.log(e);
+    }
     if (window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveBlob(blob, filename);
+      window.navigator.msSaveBlob(file, filename);
     } else {
       const elem = window.document.createElement('a');
-      elem.href = window.URL.createObjectURL(blob);
+      elem.href = window.URL.createObjectURL(file);
       elem.download = filename;
       document.body.appendChild(elem);
       elem.click();
