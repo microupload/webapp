f6b69ccea7d4b804a038d4b0cfe2888e4e535154
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 23:55:36 -0500

    Implemented file decryption

--- src/app/modules/crypto/services/crypto.service.ts
+++ src/app/modules/crypto/services/crypto.service.ts
@@ -11,25 +11,21 @@ export class CryptoService {
 
   constructor() { }
 
-  public set pub(pub: File) {
-    (async () => {
-      try {
-        this._pub = await this.readFile(pub);
-      } catch (e) {
-        this._pub = undefined;
-      }
-    })();
+  public async setPub(pub: File) {
+    try {
+      this._pub = await this.readFile(pub);
+    } catch (e) {
+      this._pub = undefined;
+    }
   }
 
-  public set priv(priv: File) {
-    (async () => {
-      try {
-        this._priv = await this.readFile(priv);
-        console.log(this._priv);
-      } catch (e) {
-        this._priv = undefined;
-      }
-    })();
+  public async setPriv(priv: File) {
+    try {
+      this._priv = await this.readFile(priv);
+      console.log(this._priv);
+    } catch (e) {
+      this._priv = undefined;
+    }
   }
 
   private async readFile(file: File): Promise<string> {
@@ -53,12 +49,24 @@ export class CryptoService {
     return forge.pki.publicKeyFromPem(this._pub);
   }
 
+  private get privateKey() {
+    return forge.pki.privateKeyFromPem(this._priv);
+  }
+
   public async encrypt(file: File) {
     const pub = this.publicKey;
     const plaintext: string = await this.readFile(file);
     const ciphertext = pub.encrypt(plaintext);
     const encrypted = new File([ciphertext], file.name, { type: file.type });
     return encrypted;
+  }
+
+  public async decrypt(file: File) {
+    const priv = this.privateKey;
+    const ciphertext: string = await this.readFile(file);
+    const plaintext = priv.decrypt(ciphertext);
+    const decrypted = new File([plaintext], file.name, { type: file.type });
+    return decrypted;
   }
 
 }
