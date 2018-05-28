10e718c78678cdbf7356d36d2e821bd3a1df5aac
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 21:03:41 -0500

    Setup basic view for uploading files

--- src/app/apps/micro-upload/pages/upload/upload.component.ts
+++ src/app/apps/micro-upload/pages/upload/upload.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
+  private file: File;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public onFileChange($event) {
+    this.file = $event.target.files[0];
+    console.log(this.file);
+  }
+
+  public get filename(): string {
+    return this.file ? this.file.name : '';
+  }
+  public get disabled(): boolean {
+    return this.file === undefined;
+  }
+
+  public onSubmit($event): void {
+    $event.preventDefault();
+    console.log(`Uploading ${this.filename}`);
+  }
 }
