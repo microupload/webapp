efbf4e7f397bd1d1689e00b0029ff3d7a326baf8
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 21:42:00 -0500

    Added a functional upload method to FileService

--- src/app/apps/micro-upload/services/file.service.spec.ts
+++ src/app/apps/micro-upload/services/file.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { FileService } from './file.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('FileService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FileService]
+      providers: [FileService],
+      imports: [
+        HttpClientModule
+      ]
     });
   });
 
--- src/app/apps/micro-upload/services/file.service.ts
+++ src/app/apps/micro-upload/services/file.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public async upload(file: File) {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+    const uploadUrl = `${environment.api}/upload`;
+    const response = await this.http.post(uploadUrl, formData, {headers}).toPromise();
+    return response;
+  }
 }
