8971264c38f98edb9b6cb96dc4a6d80e319e6575
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 22:45:15 -0500

    Providing CryptoService to CryptoModule

--- src/app/modules/crypto/crypto.module.ts
+++ src/app/modules/crypto/crypto.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CryptoService } from './services/crypto.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    CryptoService
+  ]
 })
 export class CryptoModule { }
