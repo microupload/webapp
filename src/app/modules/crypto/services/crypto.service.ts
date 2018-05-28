0f7bb599f54a6ca8ba1ca799c497e91c65421a69
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 22:57:18 -0500

    Added getter/setter for public key in CryptoService

--- src/app/modules/crypto/services/crypto.service.ts
+++ src/app/modules/crypto/services/crypto.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class CryptoService {
 
+  private _pub: string;
+
   constructor() { }
+
+  public set pub(pub: File) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this._pub = (<any>e.target).result;
+    };
+    reader.readAsText(pub);
+  }
+
+  private get pubkey(): string {
+    return this._pub;
+  }
+
 }
