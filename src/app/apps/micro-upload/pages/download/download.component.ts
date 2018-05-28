f79d50e776548cc9a3f004156aad3ea4ec72b682
John A. Fedoruk <johnny@johnfedoruk.ca>
2018-05-27 23:56:33 -0500

    Download and decrypt with RSA

--- src/app/apps/micro-upload/pages/download/download.component.ts
+++ src/app/apps/micro-upload/pages/download/download.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FileService } from '../../services/file.service';
+import { CryptoService } from '../../../../modules/crypto/services/crypto.service';
 
 @Component({
   selector: 'app-download',
@@ -11,10 +12,15 @@ export class DownloadComponent implements OnInit {
 
   public title = 'Downloading...';
   public downloaded = false;
+  public priv: File;
+
+  private data: any;
+  private metadata: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private fileService: FileService
+    private fileService: FileService,
+    private crypto: CryptoService
   ) { }
 
   ngOnInit() {
@@ -22,20 +28,29 @@ export class DownloadComponent implements OnInit {
       async params => {
         const id: string = params.id;
         if (id !== undefined) {
-          const data = await this.fileService.download(id);
-          const metadata = await this.fileService.downloadMetadata(id);
-          this.createDownloadLink(data, metadata);
-          this.downloaded = true;
-          this.title = (<any>metadata).filename;
+          this.data = await this.fileService.download(id);
+          this.metadata = await this.fileService.downloadMetadata(id);
+          this.title = (<any>this.metadata).filename;
         }
       }
     );
   }
 
-  private createDownloadLink(data, metadata) {
+  public async onKeyChange($event) {
+    this.priv = $event.target.files[0];
+    await this.crypto.setPriv(this.priv);
+
+    this.createDownloadLink(this.data, this.metadata);
+  }
+
+  public get privname(): string {
+    return this.priv ? this.priv.name : '';
+  }
+
+  private async createDownloadLink(data, metadata) {
     const filename = metadata.filename;
     const type = metadata.mimetype;
-    const blob = new Blob([data], { type });
+    const blob = await this.crypto.decrypt( new File([data], metadata.filename, { type }) );
     if (window.navigator.msSaveOrOpenBlob) {
       window.navigator.msSaveBlob(blob, filename);
     } else {
